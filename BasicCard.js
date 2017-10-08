664cea56b26de76c4f881f0635ecf37edcab385c
DESARE GAGO <desaregago@DESAREs-MacBook-Pro.local>
2017-10-08 11:42:33 -0400

    new files

---
+++ BasicCard.js
@@ -0,0 +1,38 @@
+//// create consructor named BasicCard
+///Create two arguments 1- front and 2- back, the constructor whould except 2 arguments 
+
+//Object should have a front property that contains the text on the front of the card
+//object should have a back property that contains the text on the back of the card
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+module.export = BasicCard;
